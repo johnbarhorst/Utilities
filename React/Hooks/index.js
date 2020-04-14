b72b4974037db8db6d9d8520b2df6c2005e68f0a
John Barhorst <crashdsinc@comcast.net>
2020-04-14 17:56:25 -0500

    All dem hooks

---
+++ React/Hooks/index.js
@@ -0,0 +1,6 @@
+export * from './useFetchData';
+export * from './useHover';
+export * from './useScrollFreeze';
+export * from './useToggle';
+export * from './useWindowWidth';
+export * from './useMeasure';
---
+++ React/Hooks/useFetchData.js
@@ -0,0 +1,52 @@
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH': return {
+      ...state,
+      isLoading: true,
+      isError: false,
+    }
+    case 'SUCCESS': return {
+      ...state,
+      isLoading: false,
+      isError: false,
+      data: action.payload
+    }
+    case 'ERROR': return {
+      ...state,
+      isLoading: false,
+      isError: true,
+      data: action.payload
+    }
+
+    default:
+      break;
+  }
+}
+
+export const useFetchData = (initialData) => {
+  const [state, dispatch] = useReducer(reducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
+
+  const goFetch = async (url, options = {}) => {
+    dispatch({ type: 'FETCH' });
+    try {
+      const req = await fetch(url, options);
+      const json = await req.json();
+      if (json.status === 200) {
+        dispatch({ type: 'SUCCESS', payload: json });
+      }
+      if (json.status === 404) {
+        dispatch({ type: 'ERROR', payload: json });
+      }
+    } catch (error) {
+      dispatch({ type: 'ERROR' });
+    }
+  }
+
+  return [state, goFetch];
+}
